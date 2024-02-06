5557bb8c19ba33bff7a1e6628565bb798519eac5
yung tech <76264891+Steeve-Crypto@users.noreply.github.com>
2024-02-05 23:59:30 -0500

    Update wordCounting.java

--- wordCounting.java
+++ wordCounting.java
@@ -12,6 +12,26 @@ public class WordCounter {
         String filePath = "words.txt"; 
         Map<String, Integer> wordCounts = new HashMap<>();
 
-        
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.split("[^a-zA-Z']+");
+                for (String word : words) {
+                    word = word.toLowerCase();
+                    if (word.length() > 3 && !isNumeric(word)) {
+                        wordCounts.put(word, wordCounts.getOrDefault(word, 0) + 1);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (Map.Entry<String, Integer> entry : wordCounts.entrySet()) {
+            System.out.println(entry.getKey() + "\t" + entry.getValue());
+        }
+    }
+    private static boolean isNumeric(String str) {
+        return str.matches("\\d+");
     }
 }
